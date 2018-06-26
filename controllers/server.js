51e838844e6d115df7f8100286051184f6a4388f
Glitch (exercise-tracxer) <none>
2018-06-26 17:33:44 +0000

    ☂️🏍 Checkpoint ./controllers/server.js:553280/1614

--- controllers/server.js
+++ controllers/server.js
@@ -4,18 +4,34 @@ var Users = require(path + '/models/users.js');
 var str = "abcde0fghij1klmno2pqrst3uvwxy4zABCD5EFGHI6JKLMN7OPQRS8TUVWX9YZ";  // string hash for random()
 
 
-for (var i = 0; i < 6; i++) {
-  short += str[Math.floor(Math.random() * str.length)];
-  }
+
 
 
 function ClickHandler ()  {
   
-    this.addUser = () => {
+    this.addUser = (req, res) => {
       
-    }
-    
+      Users.find(req.body), (err, user) => {
+        if(err) throw err;
+        if(user) {
+          console.log(user);
+          res.json(user);
+        } else {
+          // create userID
+          let password = 'XTuser-';    
+          for (var i = 0; i < 6; i++) {
+            password += str[Math.floor(Math.random() * str.length)];
+          }
+          
+          var newUser = new User();
+          newUser.username = req.body.username;
+          newUser.userId = password;
+          
+          newUser.save( err => {
+            if(err) throw err;
     
 };
 
 module.exports = ClickHandler;
+      
+      
