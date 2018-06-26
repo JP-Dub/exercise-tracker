0c329d3f1b273b2c0d4f650c3deeba6d6ca5b437
Glitch (exercise-tracxer) <none>
2018-06-26 21:57:30 +0000

    ⛳️💎 Checkpoint ./controllers/server.js:553280/18

--- controllers/server.js
+++ controllers/server.js
@@ -53,7 +53,7 @@ function ClickHandler ()  {
   
     this.printLog = (req, res) => {
       //console.log(req.query, req.params)
-      Users.findOne({}).select().limit(), (err, user) => {
+      Users.findOne({}, (err, user) => {
         if(err) throw err;
         console.log(user)
       });
