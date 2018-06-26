292b9078e56e256b43ab700b687efae572248afa
Glitch (exercise-tracxer) <none>
2018-06-26 17:00:56 +0000

    🏰🔑 Checkpoint ./controllers/server.js:553280/142

--- controllers/server.js
+++ controllers/server.js
@@ -1,8 +1,14 @@
 'use strict';
-
-var Users = require('/models/users.js');
+var path = process.cwd();
+var Users = require(path + '/models/users.js');
 
 function ClickHandler ()  {
   
+    this.addUser = () => {
+      
+    }
     
-}
+    
+};
+
+module.exports = ClickHandler;
