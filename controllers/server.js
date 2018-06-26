937061800a9987db788567daaaea898422b7917c
Glitch (exercise-tracxer) <none>
2018-06-26 17:22:47 +0000

    🐒⏳ Checkpoint ./controllers/server.js:553280/262

--- controllers/server.js
+++ controllers/server.js
@@ -1,6 +1,13 @@
 'use strict';
 var path = process.cwd();
 var Users = require(path + '/models/users.js');
+var str = "abcde0fghij1klmno2pqrst3uvwxy4zABCD5EFGHI6JKLMN7OPQRS8TUVWX9YZ";  // string hash for random()
+
+
+for (var i = 0; i < 6; i++) {
+  short += str[Math.floor(Math.random() * str.length)];
+  }
+
 
 function ClickHandler ()  {
   
