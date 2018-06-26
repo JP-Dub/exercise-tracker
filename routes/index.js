9e57e0ed281e010cb01213208fb586a42918a5f8
Glitch (exercise-tracxer) <none>
2018-06-26 14:17:46 +0000

    🌒🎇 Checkpoint ./routes/index.js:553280/448 ./server.js:553280/2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function(apps) {
+module.exports = function(app) {
+  
+  app.route('/api/exercise/:new-user')
+      .post( (req, res) => {
+        console.log(req.body, req.path)
+      });
+  
+  app.route('/api/exercise/:add')
+  app.post('/api/exercise/:add', (req, res) => {
+        console.log(req.body);
+      });
+  
   
 };
